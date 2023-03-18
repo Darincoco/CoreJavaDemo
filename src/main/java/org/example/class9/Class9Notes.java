b18c608948693b65b9fc8e14112e7c2ce74ec213
Darincoco <darincoco@gmail.com>
2023-03-17 23:44:06 -0400

    update notes and hws

---
+++ src/main/java/org/example/class9/Class9Notes.java
@@ -0,0 +1,28 @@
+package org.example.class9;
+
+public class Class9Notes {
+/*
+1. Spring Core, Spring MVC, Spring Boot
+    Spring core: set up servlet, xml by hand, complex
+    Spring MVC: set only one dispatcher servlet
+    Spring boot: embedded Tomcat/ starter / auto config
+
+
+2. Spring MVC(Model View Controller)
+    DispatcherServlet
+
+
+3. Spring Boot
+    advantage: autoconfig, dependency management: starter, embedded server: Tomcat(default), annotation based
+
+
+4. REST API design
+
+
+5. Map json to object: @RequestBody  json -> java object, @ResponseBody  java object -> json(in @RestController)
+    @Controller vs @RequestController, @Service, @Repository
+    @RequestController contains @Controller and @ResponseBody
+    @PathVariable, @RequestParam: retrieve id from question mark
+    @RequestParam is used to extract query parameters while @PathVariable is used to extract data right from the URI
+ */
+}
