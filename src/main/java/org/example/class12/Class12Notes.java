54cd67f1684328f263f4cb65cb9cd30c996f090f
Darincoco <darincoco@gmail.com>
2023-03-27 19:47:28 -0400

    update notes and hws

--- src/main/java/org/example/class12/Class12Notes.java
+++ src/main/java/org/example/class12/Class12Notes.java
@@ -47,5 +47,47 @@ public class Class12Notes {
     Liskov Substitution Principle
     Interface Segregation Principle
     Dependency Inversion Principle
+
+
+7. Microservice components
+    DNS: map domain name to IP address
+    LB(load balancer): route different requests to web services
+    Database
+    Data firehose: used to do some data processing, a data engineer work part
+    Cache Service
+    job queue/service: do some offline work
+    Services
+    cloud storage
+    full text search:
+        traditional schema:
+        id  |    title
+        1   | I like dogs
+        2   | dogs are cute
+        3   | I like cats
+
+        inverted index(used in full text search service):
+        keyword  | ids
+        dog      | 1, 2
+        cute     | 2
+        like     | 1,3
+    Data warehouse: A data warehouse is a system that collects data from many sources and distributes it to an
+    organization for analysis and reporting.
+        Data warehouse vs database:
+            Data warehouse is used for Analytical, database is for Transactional and Operational
+            The database is basically a collection of data that is totally application-oriented.
+            The data warehouse, in contrast, focuses on a certain type of data.
+    CDN (Content Delivery Network): A CDN is a network of servers that distributes content from an “origin” server
+    throughout the world by caching content close to where each end user is accessing the internet via a web-enabled device.
+
+
+8. Spring Cloud
+    Config server: Config server
+    Service discovery: Eureka, Consoul, Zookeeper
+    Zipkin and Sleuth: tracing system
+    Ribbon: Client side load balancer
+    ELK: Kibana, Logstash, Elastic Search
+    Circuit Breaker: (default tolerance proxy) fall back
+        Hystrix -> Resilience4j
+
  */
 }
