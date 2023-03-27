724614a4dd70196657f6de082817a626c53026d5
Darincoco <darincoco@gmail.com>
2023-03-27 01:20:34 -0400

    update notes and hws

---
+++ src/main/java/org/example/class12/Class12Notes.java
@@ -0,0 +1,51 @@
+package org.example.class12;
+
+public class Class12Notes {
+/*
+1. Web Architecture
+    Web application and web service
+        web app: Amazon.com, GUI
+        web service: one component of web app
+
+
+2. Evolution of web architecture
+    Monolithic: backend frontend all in server. Not convenient for modifying
+    Front-back separation: separate back end and front end,  client side to front end, front end to backend or vice versa
+    Way to communicate:
+        front end send request to back end for data
+        web-socket: connection between back end front end, back end doesn't need to wait for request,
+        push data directly to front end through web-socket
+    Service oriented architecture:
+        ESB: enterprise service bus
+    Microservice:
+        each service has its own database
+    Mirco-Front:
+        webpack
+
+
+3. Communication
+    REST: json, xml, html
+    SOAP: xml
+    RPC: remote procedure call
+    GraphQL: pull data from multiple data resource with single call
+
+
+4. data store
+    sql: Mysql, Oracle, Sql server...
+    no-sql: mongoDB, Cassandra
+    cache: redis, mem-cache
+
+
+5. Operating System
+    Linux,
+    Windows, Mac
+
+
+6. SOLID Principle
+    Single Responsibility Principle
+    Open-closed Principle
+    Liskov Substitution Principle
+    Interface Segregation Principle
+    Dependency Inversion Principle
+ */
+}
